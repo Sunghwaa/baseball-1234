422fc2bfd932ea19181c5b7c7159515596aaefa5
Sunghwaa <sunghwaa.lee@gmail.com>
2024-06-13 14:06:27 +0900

    [feature] 39

--- game.py
+++ game.py
@@ -5,3 +5,7 @@ class Game:
 
         if len(guessNumber) != 3:
             raise TypeError()
+
+        for char in guessNumber:
+            if not char.isdigit():
+                raise TypeError()
