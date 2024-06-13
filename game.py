edd392ba375a4935ca37eb85ee6eec70aeef208a
Sunghwaa <sunghwaa.lee@gmail.com>
2024-06-13 14:11:45 +0900

    [refactor] 43

--- game.py
+++ game.py
@@ -1,16 +1,14 @@
 class Game:
     def guess(self, guessNumber):
+        self.assert_illegal_value(guessNumber)
+
+    def assert_illegal_value(self, guessNumber):
         if guessNumber is None:
             raise TypeError()
-
         if len(guessNumber) != 3:
             raise TypeError()
-
         for char in guessNumber:
             if not char.isdigit():
                 raise TypeError()
-
-        if guessNumber[0] == guessNumber[1] or \
-                guessNumber[0] == guessNumber[2] or \
-                guessNumber[1] == guessNumber[2]:
+        if len(guessNumber) != len(set(guessNumber)):
             raise TypeError()
